4cd775a2ebfcceba7acd546e3af34b07b02c46d0
rankitbishnoi <rankitbishnoi@users.noreply.github.com>
2017-11-02 09:57:47 +0530

    added java script file

--- js/main.js
+++ js/main.js
@@ -1,36 +1,9 @@
+var form = document.forms.contactForm;
 
-		$(function(){
+form.addEventListener ("submit", reply, false);
 
-			/*-------------------------------
-
-				GENERAL EXAMPLES
-
-			-------------------------------*/
-
-			
-			// HTML for content
-			$('.html_popup').popup({
-				content		: '<h1>This is some HTML</h1>',
-				type		: 'html'
-			});
-
-		});
-			
-
-		/*---------------------
-
-			JQUERY EASING
-
-		*/
-
-		$.extend($.easing, {
-			easeOutBack: function (x, t, b, c, d, s) {
-				if (s == undefined) s = 1.70158;
-				return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-			},
-			easeInBack: function (x, t, b, c, d, s) {
-				if (s == undefined) s = 1.70158;
-				return c*(t/=d)*t*((s+1)*t - s) + b;
-			}
-		});
+function reply() {
+  alert("Submitted");
+  return false;
+}
 	
